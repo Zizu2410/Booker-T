a97c70f64fdffe0cbc3548924cbf3ee523d7bfcf
Alwaz <alwazkazi3@gmail.com>
2021-06-01 13:48:11 -0700

    add signup login

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import { Counter } from './features/counter/Counter';
-import './App.css';
-import Header from './components/header/Header';
-import Banner from './components/banner/Banner';
-import Featured from './components/featured/Featured';
+import React from "react";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import Header from "./components/header/Header";
+import Banner from "./components/banner/Banner";
+import Featured from "./components/featured/Featured";
+import SignUp from "./components/screens/signupscreen/Signup";
+import Login from "./components/screens/loginscreen/Login";
 
 function App() {
   return (
     <div className="App">
-       <Header />
-       <Banner />
-       <Featured />
+      {/* <Header /> */}
+      {/* <SignUp /> */}
+      <Login />
+      {/* <Banner />
+      <Featured /> */}
     </div>
   );
 }
