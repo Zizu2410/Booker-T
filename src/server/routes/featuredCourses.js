32275609a61820d168b64ef6f2a1e213aa0f9dd7
tribe-05-jadu <samitkarim9@gmail.com>
2021-06-06 20:13:11 +0400

    postman success

--- src/server/routes/featuredCourses.js
+++ src/server/routes/featuredCourses.js
@@ -1,10 +1,25 @@
 const express= require('express')
+const { db } = require('../models/FeaturedCourses')
 
 const router = express.Router()
-const course = require('../models/FeaturedCourses')
+const Course = require('../models/FeaturedCourses')
 
-router.get('/', (req,res)=>{
-    res.send('ok')
+router.get('/',async (req,res)=>{
+    try{
+        const courses= await db.collection('AllCourses').findOne()
+        res.json(courses)
+    }catch(err){
+        res.json({message:err})
+    }
+})
+
+router.get(':featuredcoursesId',async (req,res)=>{
+    try{
+        const courses = await db.collection('AllCourses').findById(req.params.featuredCoursesId)
+        res.json(courses)
+    }catch(err){
+        res.json({message:err})
+    }
 })
 
 module.exports=router
